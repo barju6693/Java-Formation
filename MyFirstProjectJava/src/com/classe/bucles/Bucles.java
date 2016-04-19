b5fe55212c7e983059d531c0cb6e27b7941070f7
barju6693 <F1barju@gmail.com>
2016-04-19 09:57:12 +0200

    Added files via upload

---
+++ MyFirstProjectJava/src/com/classe/bucles/Bucles.java
@@ -0,0 +1,59 @@
+package com.classe.bucles;
+
+public class Bucles {
+
+	public void count(int num1, int num2){
+		// entra dos nombres i compto quant n'hi ha pel mig
+		int counter=0;
+		for (int i=num1;i<num2;i++){
+			counter++;
+		}
+		System.out.println("Hi han "+counter+" nombres");
+	}
+	
+	public void mentre(int num1, int num2){
+		int counter=0;
+		while(num1<num2){
+			num1++;
+			counter++;
+		}
+		while(num2<num1){
+			num2++;
+			counter++;
+		}
+		System.out.println("Hi han "+counter+" nombres");
+	}
+	
+	public void fesMentre(int num1, int num2){
+		int counter=0;
+		do{
+			num1++;
+			counter++;
+		}while(num1<num2);
+		
+		if(num1>num2){
+			counter=0;
+			num1--;
+		}
+		
+		do{
+			num2++;
+			counter++;
+		}while(num2<num1);
+		System.out.println(counter);
+	}
+	
+	public void multFor(int mult){
+		for (int i=0; i<11;i++){
+			System.out.println(i*mult);
+		}
+	}
+	
+	public void multWhile(int mult){
+		int i=0;
+		while (i<11){
+			System.out.println(i*mult);
+			i++;
+		}
+	}
+}
---
+++ MyFirstProjectJava/src/com/classe/bucles/Mainç.java
@@ -0,0 +1,17 @@
+package com.classe.bucles;
+
+public class Mainç {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Bucles buc = new Bucles();
+		buc.fesMentre(1, 7);
+		buc.fesMentre(7, 1);
+		buc.mentre(1, 7);
+		buc.mentre(7, 1);
+		buc.multFor(5);
+		buc.multWhile(5);
+	}
+
+}
